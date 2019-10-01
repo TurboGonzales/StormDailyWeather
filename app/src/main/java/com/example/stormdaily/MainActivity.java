48c4d3b62bec975e0bc2c664b2231f0a7c021933
Viacheslav <zex96@me.com>
2019-10-01 03:16:41 +0300

    2 laba dodelana

--- app/src/main/java/com/example/stormdaily/MainActivity.java
+++ app/src/main/java/com/example/stormdaily/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.stormdaily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton refresher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refresher = findViewById(R.id.refresher);
+        refresher.setOnClickListener(this); //присваиваем обработчик кнопкам
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.refresher) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Обновление данных")
+                    .setMessage("Вы хотите обновить информацию о погоде?")
+                    .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                            int id) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setCancelable(false)
+                    .setNegativeButton("Нет, хочу смотреть на старье",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+
 }
